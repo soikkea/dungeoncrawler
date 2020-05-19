658301715ead952bc69299d4796bd3c93fd54368
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-05-19 19:26:39 +0300

    Add Item and Weapon classes

---
+++ source/headers/item.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+
+class Creature;
+
+class Item
+{
+public:
+	Item(std::string name, int weight);
+
+	virtual bool onInteract(Creature& creature) { return false; }
+	virtual bool onUse(Creature& target) { return false; }
+
+private:
+	std::string _name;
+	int _weight;
+};
+
+class Weapon : public Item
+{
+public:
+	Weapon(std::string name, int weight, int damage, int baseHitChance);
+
+	bool onInteract(Creature& creature) override;
+	bool onUse(Creature& target) override;
+
+	const int getDamage() const;
+
+private:
+	int _damage;
+	int _baseHitChance;
+};
---
+++ source/src/item.cpp
@@ -0,0 +1,43 @@
+#include "item.h"
+#include "creature.h"
+#include "rng.h"
+
+Item::Item(std::string name, int weight) :
+	_name(name),
+	_weight(weight)
+{
+}
+
+Weapon::Weapon(std::string name, int weight, int damage, int baseHitChance) :
+	Item(name, weight),
+	_damage(damage),
+	_baseHitChance(baseHitChance)
+{
+	if (_baseHitChance < 0)
+		_baseHitChance = 0;
+	if (_baseHitChance > 100)
+		_baseHitChance = 100;
+}
+
+bool Weapon::onInteract(Creature& creature)
+{
+	// TODO: Equip the weapon
+	return false;
+}
+
+bool Weapon::onUse(Creature& target)
+{
+	int hitThrow = rng::randomIntBetween(1, 100);
+	if (hitThrow > _baseHitChance)
+		return false;
+
+	target.gainHitpoints(-_damage);
+
+	return true;
+}
+
+const int Weapon::getDamage() const
+{
+	return _damage;
+}
+
