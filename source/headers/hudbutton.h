834f722e34e0d58e04e6aea50d0ff447c89efa5f
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-07-12 14:57:08 +0300

    Add menu button

--- source/headers/hudbutton.h
+++ source/headers/hudbutton.h
@@ -6,12 +6,11 @@
 
 class HudButton : public sf::Drawable, public ::sf::Transformable {
 public:
-	HudButton(const sf::Vector2f& position, const sf::Rect<float>& boundingBox, const std::string& name);
+	HudButton(const sf::Vector2f& position, const std::string& name);
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 	sf::Vector2f position;
-	sf::Rect<float> boundingBox;
 	sf::Sprite sprite;
 	bool active = false;
 	std::string name;
@@ -21,4 +20,14 @@ class IncrementButton : public HudButton {
 public:
 	IncrementButton(const sf::Vector2f& position, const std::string& name);
 	static const sf::Vector2f SIZE;
+};
+
+class MenuButton : public HudButton {
+public:
+	MenuButton(const sf::Vector2f& position, const std::string& name, const std::string& text);
+	static const sf::Vector2f SIZE;
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+protected:
+	std::string text;
+	sf::Text textObject;
 };
--- source/src/hudbutton.cpp
+++ source/src/hudbutton.cpp
@@ -1,10 +1,8 @@
 #include "hudbutton.h"
 #include "assets.h"
 
-HudButton::HudButton(const sf::Vector2f& position,
-	const sf::Rect<float>& boundingBox, const std::string& name) :
+HudButton::HudButton(const sf::Vector2f& position, const std::string& name) :
 	position(position),
-	boundingBox(boundingBox),
 	name(name)
 {
 	setPosition(position);
@@ -20,8 +18,29 @@ void HudButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
 const sf::Vector2f IncrementButton::SIZE = sf::Vector2f(12.f, 12.f);
 
 IncrementButton::IncrementButton(const sf::Vector2f& position, const std::string& name) :
-	HudButton(position, sf::Rect<float>(position, SIZE), name)
+	HudButton(position, name)
 {
 	sprite = sf::Sprite(Assets::get().getTexture("increment_button"));
 	sprite.setPosition(position);
+}
+
+const sf::Vector2f MenuButton::SIZE = sf::Vector2f(300.f, 100.f);
+
+MenuButton::MenuButton(const sf::Vector2f& position, const std::string& name, const std::string& text) :
+	HudButton(position, name),
+	text(text)
+{
+	sprite = sf::Sprite(Assets::get().getTexture("menu_button"));
+	sprite.setPosition(position);
+	active = true;
+	textObject = sf::Text(text, Assets::get().getFont("main"), 16);
+	textObject.setPosition(position);
+}
+
+void MenuButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	if (!active)
+		return;
+	HudButton::draw(target, states);
+	target.draw(textObject);
 }
